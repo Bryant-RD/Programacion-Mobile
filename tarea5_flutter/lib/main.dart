019139bff46a9aa4dd903d8b42f0a35bf414f8a4
Bryant-RD <briant04.rt@gmail.com>
2023-10-19 08:36:42 -0400

    cambiando contador por formulario simple

--- tarea5_flutter/lib/main.dart
+++ tarea5_flutter/lib/main.dart
@@ -1,52 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  int contador = 0;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blueAccent
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text("Contador"),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("$contador", style: const TextStyle(fontSize: 140, fontWeight: FontWeight.w100)),
-              const Text("clicks", style: TextStyle(fontSize: 25)),
+    return const MaterialApp(
+      home: MyForm(),
+    );
+  }
+}
 
-            ],
-          )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            
-            setState(() {
-              contador++;
-            });
-            
-          },
-          child: const Icon(Icons.plus_one))
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  String name = "";
+  String career = "";
+  int age = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Formulario Flutter'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              'assets/perfil.jpg',
+              width: 150.0,
+              height: 150.0,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Nombre'),
+              onChanged: (value) {
+                setState(() {
+                  name = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Carrera'),
+              onChanged: (value) {
+                setState(() {
+                  career = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Edad'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  age = int.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Acción para mostrar la información ingresada
+                _showInfoDialog();
+              },
+              child: const Text('Mostrar información'),
+            ),
+            const SizedBox(height: 16),
+            const Text('Información ingresada:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 8),
+            Text('Nombre: $name', style: const TextStyle(fontSize: 16)),
+            Text('Carrera: $career', style: const TextStyle(fontSize: 16)),
+            Text('Edad: $age', style: const TextStyle(fontSize: 16)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showInfoDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Información ingresada'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Nombre: $name'),
+              Text('Carrera: $career'),
+              Text('Edad: $age'),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cerrar'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
